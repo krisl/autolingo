2242d6882639fde8e8ef8cb8f5771737cea08992
2-X <brethower.k@gmail.com>
2020-09-06 12:16:19 -0400

    update comment

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ const set_badge = (is_enabled) => {
     chrome.browserAction.setBadgeBackgroundColor({color: is_enabled ? "green" : "#EC5053"});
 }
 
-// check user credentials every .5s
+// check if the extension is enabled and set the badge accordingly
 setInterval(() => {
     chrome.storage.local.get(
         "autolingo_enabled",
